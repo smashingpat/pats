557cf1279c3d1f09f0d332a61a17c11c03553bb1
smashingpat <patt.gerr@gmail.com>
2017-09-14 09:20:39 +0200

    initialized project

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "indent": [2, 4],
+    },
+};
---
+++ index.js
@@ -0,0 +1,6 @@
+const Pats = require('./lib');
+
+
+Pats({
+    rootDir: __dirname,
+});
